7e558b75054e6f1047a6b7535e77f5fc7f638706
Atsushi Neki <atsushin@fast.au.fujitsu.com>
2020-10-05 17:12:51 +1100

    Initial commit

---
+++ src/containers/AuctionBuilder/AuctionBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+
+class AuctionBuilder extends Component {
+  render () {
+    return (
+      <Aux>
+        <div>Auction</div>
+        <div>Auction Control</div>
+      </Aux>
+    )
+  }
+}
+
+export default AuctionBuilder;
